a431ea0540c911b7d628edfb46f24c1c70e77ffd
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-09-17 16:54:41 -0600

    test: validations

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,8 @@ class Ride < ApplicationRecord
   belongs_to :amusement_park
   has_many :ride_mechanics
   has_many :mechanics, through: :ride_mechanics
+  
+  validates :name, presence: true
+  validates :thrill_rating, numericality: true
+  validates :open, inclusion: [true, false]
 end
