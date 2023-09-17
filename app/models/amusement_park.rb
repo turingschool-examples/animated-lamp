7cfc5f25ce33757ab5ae85e5b88b8b5c392799c2
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-17 14:48:34 -0600

    feat: add find_mechanics model method to amusement park model

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,3 +1,8 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
+  has_many :mechanics, through: :rides
+
+  def find_mechanics
+    AmusementPark.joins(:mechanics).where("amusement_parks.id = ?", self.id).distinct.pluck("mechanics.name")
+  end
 end
