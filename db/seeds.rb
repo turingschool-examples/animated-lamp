4785ae20624880caad32c5c3276ec604c20037bc
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-17 14:49:01 -0600

    feat: add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,26 @@
 AmusementPark.destroy_all
 Ride.destroy_all
 
-@six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
-@universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
+six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
 
-@hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
-@scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
-@ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+hurler = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+scrambler = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+ferris = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
 
-@jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+jaws = universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+
+mechanic_1 = Mechanic.create!(
+  name: "Frank Fixes",
+  years_experience: 12
+)
+
+mechanic_2 = Mechanic.create!(
+  name: "Abby Awesome",
+  years_experience: 2
+)
+
+RideMechanic.create!(mechanic: mechanic_1, ride: hurler)
+RideMechanic.create!(mechanic: mechanic_1, ride: scrambler)
+RideMechanic.create!(mechanic: mechanic_2, ride: scrambler)
+RideMechanic.create!(mechanic: mechanic_2, ride: ferris)
