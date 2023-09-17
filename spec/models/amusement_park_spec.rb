b7e120926afcc6489511fdd6cf8105c6918591ea
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-17 15:45:12 -0600

    test: add test for ride_avg_experience amusement_park model methods

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -49,4 +49,57 @@ RSpec.describe AmusementPark, type: :model do
       expect(amusement_park_1.find_mechanics).to eq(["#{mechanic_1.name}", "#{mechanic_2.name}"].sort)
     end
   end
+
+  describe "#ride_avg_experience" do
+    it "returns the average years of experience for an amusement park's rides mechanic's in order of experience" do
+      amusement_park_1 = AmusementPark.create!(
+        name: "Fun World",
+        admission_cost: 75
+      )
+      
+      ride_1 = amusement_park_1.rides.create!(
+        name: "Twisty Turny",
+        thrill_rating: 8,
+        open: true
+      )
+
+      ride_2 = amusement_park_1.rides.create!(
+        name: "Slowy Slide",
+        thrill_rating: 3,
+        open: true
+      )
+
+      mechanic_1 = Mechanic.create!(
+        name: "Frank Fixes",
+        years_experience: 12
+      )
+
+      mechanic_2 = Mechanic.create!(
+        name: "Abby Awesome",
+        years_experience: 2
+      )
+
+      mechanic_3 = Mechanic.create!(
+        name: "Eliza Engineer",
+        years_experience: 25
+      )
+
+      mechanic_4 = Mechanic.create!(
+        name: "Bob Builder",
+        years_experience: 40
+      )
+
+      RideMechanic.create!(ride: ride_1, mechanic: mechanic_1)
+      RideMechanic.create!(ride: ride_1, mechanic: mechanic_2)
+      RideMechanic.create!(ride: ride_2, mechanic: mechanic_3)
+      RideMechanic.create!(ride: ride_2, mechanic: mechanic_4)
+
+      rides = amusement_park_1.ride_avg_experience
+      
+      expect(rides[0].ride_name).to eq("#{ride_1.name}")
+      expect(rides[1].ride_name).to eq("#{ride_2.name}")
+      expect(rides[0].avg_experience).to eq(7.0)
+      expect(rides[1].avg_experience).to eq(32.5)
+    end
+  end
 end
