79682ec879682333dafe91c790a08bafd19b23d1
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-17 15:44:45 -0600

    feat: add rides section to amusement park show view, add ride_avg_experience method to amusement park model

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -5,4 +5,8 @@ class AmusementPark < ApplicationRecord
   def find_mechanics
     AmusementPark.joins(:mechanics).where("amusement_parks.id = ?", self.id).distinct.pluck("mechanics.name")
   end
+
+  def ride_avg_experience
+    AmusementPark.joins(:mechanics).select("avg(mechanics.years_experience) AS avg_experience, rides.name AS ride_name").where("amusement_parks.id = ?", self.id).group(:ride_name).order(:avg_experience)
+  end
 end
