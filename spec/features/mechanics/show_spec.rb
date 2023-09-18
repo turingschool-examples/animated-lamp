1be2b4979196c26973c9d041121120a6fb0e3539
Lane Bretschneider <lanebret85@gmail.com>
2023-09-17 19:52:21 -0700

    test: add test for form to add a ride to a mechanic

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe "Mechanics Show Page", type: :feature do
     
     @twister = @elitch_gardens.rides.create!(name: "Twister", thrill_rating: 6, open: true)
     @carousel = @elitch_gardens.rides.create!(name: "Carousel", thrill_rating: 2, open: true)
+    @dragon_force = @elitch_gardens.rides.create!(name: "Dragon Force", thrill_rating: 10, open: true)
 
     MechanicRide.create!(mechanic: @sharon, ride: @twister)
     MechanicRide.create!(mechanic: @sharon, ride: @carousel)
@@ -26,6 +27,30 @@ RSpec.describe "Mechanics Show Page", type: :feature do
         expect(page).to have_content("Rides #{@sharon.name} is working on:")
         expect(page).to have_content(@sharon.rides.first.name)
         expect(page).to have_content(@sharon.rides.last.name)
+      end
+
+      it "displays a form to add a ride to their workload" do
+        visit "/mechanics/#{@sharon.id}"
+
+        expect(page).to have_content("Add a ride to #{@sharon.name}'s workload:")
+        expect(page).to have_content("Ride id:")
+        expect(page).to have_button("Submit")
+      end
+
+      describe "When I fill in that field with an id of an existing ride and click 'Submit'" do
+        it "takes me back to that mechanics show page and I see the name of that newly added ride on this mechanics show page" do
+          visit "/mechanics/#{@sharon.id}"
+
+          expect(page).to_not have_content(@dragon_force.name)
+
+          fill_in :ride_id, with: @dragon_force.id
+
+          click_on "Submit"
+
+          expect(current_path).to eq("/mechanics/#{@sharon.id}")
+
+          expect(page).to have_content(@dragon_force.name)
+        end
       end
     end
   end
