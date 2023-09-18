e62f7a8cc0e3d72fb02c7601b6fede5e99b9f906
Lane Bretschneider <lanebret85@gmail.com>
2023-09-17 19:53:01 -0700

    feat: add route for post '/mechanics/:mechanic_id/rides'

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/mechanics/:id", to: "mechanics#show"
+
+  post "/mechanics/:mechanic_id/rides", to: "mechanic_rides#create"
 end
