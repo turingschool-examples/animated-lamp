4e67eb6a65427613d6c7e19add708af47607a7da
Lane Bretschneider <lanebret85@gmail.com>
2023-09-17 21:03:35 -0700

    test: add test for amusement parks show page displaying avg experience for the mechanics working on each ride there

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -2,8 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Amusement Parks Show Page", type: :feature do
   before :each do
-    @sharon = Mechanic.create!(name: "Sharon", years_experience: 15)
+    @sharon = Mechanic.create!(name: "Sharon", years_experience: 12)
     @maria = Mechanic.create!(name: "Maria", years_experience: 20)
+    @geraldine = Mechanic.create!(name: "Geraldine", years_experience: 4)
     
     @elitch_gardens = AmusementPark.create!(name: "Elitch Gardens", admission_cost: 65)
     
@@ -12,9 +13,11 @@ RSpec.describe "Amusement Parks Show Page", type: :feature do
     @dragon_force = @elitch_gardens.rides.create!(name: "Dragon Force", thrill_rating: 10, open: true)
 
     MechanicRide.create!(mechanic: @sharon, ride: @twister)
-    MechanicRide.create!(mechanic: @sharon, ride: @carousel)
     MechanicRide.create!(mechanic: @maria, ride: @twister)
-    MechanicRide.create!(mechanic: @maria, ride: @carousel)
+    MechanicRide.create!(mechanic: @sharon, ride: @carousel)
+    MechanicRide.create!(mechanic: @geraldine, ride: @carousel)
+    MechanicRide.create!(mechanic: @maria, ride: @dragon_force)
+    MechanicRide.create!(mechanic: @geraldine, ride: @dragon_force)
   end
 
   describe "As a visitor" do
@@ -27,6 +30,18 @@ RSpec.describe "Amusement Parks Show Page", type: :feature do
         expect(page).to have_content("Mechanics working on rides at #{@elitch_gardens.name}:")
         expect(page).to have_content(@sharon.name)
         expect(page).to have_content(@maria.name)
+      end
+
+      it "displays a list of all of the parks rides, and next to the ride name I see the average experience of the mechanics working on the ride, and I see the list of rides is ordered by the average experience of mechanics working on the ride" do
+        visit "/amusement_parks/#{@elitch_gardens.id}"
+
+        expect(page).to have_content("#{@elitch_gardens.name}'s Rides:")
+        expect(page).to have_content("#{@twister.name}, Mechanic Average Experience: 16")
+        expect(page).to have_content("#{@carousel.name}, Mechanic Average Experience: 8")
+        expect(page).to have_content("#{@dragon_force.name}, Mechanic Average Experience: 12")
+
+        expect(@carousel.name).to appear_before(@dragon_force.name)
+        expect(@dragon_force.name).to appear_before(@twister.name)
       end
     end
   end
