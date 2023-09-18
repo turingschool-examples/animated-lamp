42bc0c514e5d5796b67a2e5739280613d3890a74
Lane Bretschneider <lanebret85@gmail.com>
2023-09-17 18:35:23 -0700

    feat: add relationships and validations

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,9 @@
 class Ride < ApplicationRecord
   belongs_to :amusement_park
+  has_many :mechanic_rides
+  has_many :mechanics, through: :mechanic_rides
+
+  validates :name, presence: true
+  validates :thrill_rating, presence: true
+  validates :open, inclusion: [true, false]
 end
