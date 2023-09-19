b7f6b99fe569737b6d3a4ce833446ef4989998c3
Antoine Aube <aaube3@gmail.com>
2023-09-18 19:26:08 -0600

    test: feature test for mechanics average years of experience

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -39,5 +39,29 @@ RSpec.describe "Amusement Park Show Page" do
         expect(page).to have_content(@Steve.name)
       end
     end 
+
+    it "displays rides at each park along with average number of years experience per mechanic working on that ride" do
+      six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+      ride1 = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+      ride2 = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+      ride3 = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+      steve = Mechanic.create!(name: 'Steve', years_experience: 5)
+      amanda = Mechanic.create!(name: 'Amanda', years_experience: 10)
+      will = Mechanic.create!(name: 'Will', years_experience: 3)
+      faith = Mechanic.create!(name: 'Faith', years_experience: 2)
+      ride_mechanic1 = RideMechanic.create!(ride: ride1, mechanic: steve)
+      ride_mechanic2 = RideMechanic.create!(ride: ride1, mechanic: amanda)
+      ride_mechanic3 = RideMechanic.create!(ride: ride2, mechanic: will)
+      ride_mechanic4 = RideMechanic.create!(ride: ride2, mechanic: steve)
+      ride_mechanic5 = RideMechanic.create!(ride: ride3, mechanic: faith)
+      ride_mechanic6 = RideMechanic.create!(ride: ride3, mechanic: will)
+      
+      visit "/amusement_parks/#{six_flags.id}"
+      expect(page).to have_content("Rides at this park:")
+      expect(page).to have_content(7.5)
+      expect(page).to have_content(4.0)
+      expect(page).to have_content(2.5)
+    end
+    
   end
 end
