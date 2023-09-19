e2973d2e6051042c51dcc37e1c8bb535c59db08f
Charles Ren <cren04@syr.edu>
2023-09-19 14:51:33 -0700

    db creation w migrations, app/model relationship associations,& model specs

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -3,5 +3,13 @@ require "rails_helper"
 RSpec.describe Ride, type: :model do
   describe "relationships" do
     it { should belong_to(:amusement_park) }
+    it { should have_many(:mechanic_rides) }
+    it { should have_many(:mechanics).through(:mechanic_rides) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :thrill_rating }
+    it { should validate_presence_of :open }
   end
 end
