f4b5fafee58b25d66d6ac861070fcc5e85350856
Will Chen <wfchen1990@gmail.com>
2023-09-19 17:53:33 -0600

    Add US3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/mechanics/:mechanic_id', to: 'mechanics#show'
   post '/mechanics/:mechanic_id/rides', to: 'ride_mechanics#create'
+
+  get '/amusementparks/:amusement_park_id', to: 'amusement_parks#show'
 end
--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -3,5 +3,26 @@ require "rails_helper"
 RSpec.describe AmusementPark, type: :model do
   describe "relationships" do
     it { should have_many(:rides) }
+    it { should have_many(:mechanics).through(:rides) }  
+  end
+
+  describe 'instance methods' do
+    describe '#unique_mechanics' do
+      it 'returns a list of unique mechanics' do
+        @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+
+        @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+        @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+        @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+
+        @mechanic_1 = Mechanic.create!(name: 'Kara Smith', years_experience: 11)
+        @mechanic_2 = Mechanic.create!(name: 'John Doe', years_experience: 5)
+
+        RideMechanic.create!(mechanic: @mechanic_1, ride: @hurler)
+        RideMechanic.create!(mechanic: @mechanic_1, ride: @scrambler)
+
+        expect(@six_flags.unique_mechanics).to eq([@mechanic_1])
+      end
+    end
   end
 end
