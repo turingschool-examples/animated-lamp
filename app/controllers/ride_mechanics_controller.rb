53e27e666f835c7db56a7ebf3578d3cf38b450c1
Antoine Aube <aaube3@gmail.com>
2023-09-18 18:13:35 -0600

    feature: form to add new ride to mechanic

--- app/controllers/ride_mechanics_controller.rb
+++ app/controllers/ride_mechanics_controller.rb
@@ -1,3 +1,9 @@
 class RideMechanicsController < ApplicationController
-  
+  def create
+    @mechanic = Mechanic.find(params[:id])
+    if params[:ride_id] != nil
+      RideMechanic.create!(ride_id: params[:ride_id], mechanic_id: params[:id])
+    end
+    redirect_to "/mechanics/#{params[:id]}"
+  end
 end
