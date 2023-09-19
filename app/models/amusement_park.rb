04f0827f4fa6378d94a54a774df0459becc2a586
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-09-19 16:26:36 -0600

    add average_ride_experience to amusement_park model

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -5,4 +5,12 @@ class AmusementPark < ApplicationRecord
   def unique_mechanics
     mechanics.distinct
   end
+
+  def average_ride_experience
+    rides
+    .joins(:mechanics)
+    .select("rides.*, AVG(mechanics.years_experience) AS average_experience")
+    .group("rides.id")
+    .order("average_experience DESC")
+  end
 end
--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -10,20 +10,31 @@ RSpec.describe AmusementPark, type: :model do
     before(:each) do
       @mechanic_1 = Mechanic.create!(name: "Kiwi Mechanic", years_experience: 2)
       @mechanic_2 = Mechanic.create!(name: "Coco Mechanic", years_experience: 1)
+      @mechanic_3 = Mechanic.create!(name: "Chicken Mechanic", years_experience: 5)
 
       @park_1 = AmusementPark.create!(name: "Chicken's Park", admission_cost: 50)
 
       @ride_1 = Ride.create!(name: "Hiccup's Coaster", thrill_rating: 1, open: true, amusement_park_id: @park_1.id)
       @ride_2 = Ride.create!(name: "Hiccup's Teacups", thrill_rating: 10, open: false, amusement_park_id: @park_1.id)
+      @ride_3 = Ride.create!(name: "Hiccup's Racing", thrill_rating: 2, open: true, amusement_park_id: @park_1.id)
 
       RideMechanic.create!(ride_id: @ride_1.id, mechanic_id: @mechanic_1.id)
       RideMechanic.create!(ride_id: @ride_2.id, mechanic_id: @mechanic_1.id)
       RideMechanic.create!(ride_id: @ride_1.id, mechanic_id: @mechanic_2.id)
+      RideMechanic.create!(ride_id: @ride_3.id, mechanic_id: @mechanic_3.id)
     end
 
     describe "#unique_mechanics" do
       it "returns a list of unique mechanics for a given park" do
-         expect(@park_1.unique_mechanics).to eq([@mechanic_1, @mechanic_2])
+         expect(@park_1.unique_mechanics).to eq([@mechanic_1, @mechanic_2, @mechanic_3])
+      end
+    end
+
+    describe "#average_ride_experience" do
+      it "returns a list of rides for the park, sorted by average ride experience" do
+        expect(@park_1.average_ride_experience).to eq([@ride_3, @ride_2, @ride_1])
+        expect(@park_1.average_ride_experience.first.average_experience).to eq(5)
+        expect(@park_1.average_ride_experience.last.average_experience).to eq(1.5)
       end
     end
   end
