22f3e1f15c5a131d65c72fbd3556e18e457f3018
Will Chen <wfchen1990@gmail.com>
2023-09-19 17:41:01 -0600

    Add US2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/mechanics/:mechanic_id', to: 'mechanics#show'
+  post '/mechanics/:mechanic_id/rides', to: 'ride_mechanics#create'
 end
--- spec/features/mechanics/show.rb
+++ spec/features/mechanics/show.rb
@@ -25,5 +25,33 @@ RSpec.feature "the mechanics show page" do
       expect(page).to have_content(@scrambler.name)
       expect(page).to_not have_content(@ferris.name)
     end
+
+    it 'US2 displays a form to add a ride to their workload' do
+      @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+      @universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
+
+      @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+      @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+      @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+
+      @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+
+      @mechanic_1 = Mechanic.create!(name: 'Kara Smith', years_experience: 11)
+
+      RideMechanic.create!(mechanic: @mechanic_1, ride: @hurler)
+      RideMechanic.create!(mechanic: @mechanic_1, ride: @scrambler)
+
+      visit "/mechanics/#{@mechanic_1.id}"
+
+      expect(page).to have_content('Add a ride to workload:')
+      expect(page).to have_button('Submit')
+
+      fill_in 'ride_id', with: @ferris.id
+      click_button 'Submit'
+
+      within '#rides' do
+        expect(page).to have_content(@ferris.name)
+      end
+    end
   end
 end
