aad7d28e6630d9619f2e46694c6706480c4281c2
Blake Sergesketter <blakesergesketter@gmail.com>
2023-09-18 19:19:47 -0500

    test: model testing for validations and relationships completed.

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe Ride, type: :model do
   describe "relationships" do
     it { should belong_to(:amusement_park) }
     it { should have_many(:mechanics).through(:ride_mechanics)}
+  end
 
+  describe "validations" do 
+    it { should validate_presence_of :name }
+    it { should allow_value(true).for(:open) }
+    it { should allow_value(false).for(:open) }
   end
 end
