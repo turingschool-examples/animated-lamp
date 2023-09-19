ac4ba64293fc3fd17a0f103945633548ee83c948
Cory Powell <coryrpowell@gmail.com>
2023-09-19 18:06:35 -0400

    added the testing for user story 1 as well as seeds for viewing in the server. Also added a route path for the #show method in mechanics, built out the view show page for mechanics and the mechanics_controller show method

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,28 @@
 AmusementPark.destroy_all
 Ride.destroy_all
 
-@six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
-@universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
+# @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+# @universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
 
-@hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
-@scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
-@ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+# @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+# @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+# @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
 
-@jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+# @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+
+@theme_park = AmusementPark.create!(name: "Itchy and Scratchy Land", admission_cost: 12)
+
+@ride_1 = @theme_park.rides.create!(name: "Puke Monster", thrill_rating: 3, open: false)
+@ride_2 = @theme_park.rides.create!(name: "Carousel", thrill_rating: 9, open: false)
+@ride_3 = @theme_park.rides.create!(name: "The Nauseator", thrill_rating: 6, open: false)
+
+
+@mechanic_1 = Mechanic.create!(name: "MC Ride", years_experience: 3)
+@mechanic_2 = Mechanic.create!(name: "Scruffy", years_experience: 4)
+@mechanic_3 = Mechanic.create!(name: "Fonzie", years_experience: 5)
+@mechanic_4 = Mechanic.create!(name: "Rosie", years_experience: 6)
+
+@workorder_1 = MechanicRide.create!(mechanic: @mechanic_1, ride: @ride_1)
+@workorder_2 = MechanicRide.create!(mechanic: @mechanic_1, ride: @ride_2)
+@workorder_3 = MechanicRide.create!(mechanic: @mechanic_3, ride: @ride_3)
+@workorder_4 = MechanicRide.create!(mechanic: @mechanic_4, ride: @ride_3) 
--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -2,12 +2,54 @@ require "rails_helper"
 
 RSpec.describe "mechanics #show" do
   before(:each) do
-    
+    @theme_park = AmusementPark.create!(name: "Itchy and Scratchy Land", admission_cost: 12)
+
+    @ride_1 = @theme_park.rides.create!(name: "Puke Monster", thrill_rating: 3, open: false)
+    @ride_2 = @theme_park.rides.create!(name: "Carousel", thrill_rating: 9, open: false)
+    @ride_3 = @theme_park.rides.create!(name: "The Nauseator", thrill_rating: 6, open: false)
 
 
+    @mechanic_1 = Mechanic.create!(name: "MC Ride", years_experience: 3)
+    @mechanic_2 = Mechanic.create!(name: "Scruffy", years_experience: 4)
+    @mechanic_3 = Mechanic.create!(name: "Fonzie", years_experience: 5)
+    @mechanic_4 = Mechanic.create!(name: "Rosie", years_experience: 6)
+
+    @workorder_1 = MechanicRide.create!(mechanic: @mechanic_1, ride: @ride_1)
+    @workorder_2 = MechanicRide.create!(mechanic: @mechanic_1, ride: @ride_2)
+    @workorder_3 = MechanicRide.create!(mechanic: @mechanic_3, ride: @ride_3)
+    @workorder_4 = MechanicRide.create!(mechanic: @mechanic_4, ride: @ride_3) 
 
   end
 
+
+  # User Story 1
+  # As a user,
+  # When I visit a mechanic show page
+  # I see their name, years of experience, and the names of all rides they are working on.
+  describe "mechanic #show" do
+    it "when I visit a mechanic show page I see their name and years of experience" do
+      visit "/mechanics/#{@mechanic_1.id}"
+
+      expect(page).to have_content(@mechanic_1.name)
+      expect(page).to have_content(@mechanic_1.years_experience)
+    end
+
+    it "and the names of all the rides they are working on." do
+      visit "/mechanics/#{@mechanic_1.id}"
+
+      expect(page).to have_content(@ride_1.name)
+      expect(page).to have_content(@ride_2.name)
+    end
+  end
+  # Story 2 - Add a Ride to a Mechanic
+
+  # As a user,
+  # When I go to a mechanic's show page
+  # I see a form to add a ride to their workload
+  # When I fill in that field with an id of an existing ride and click Submit
+  # I’m taken back to that mechanic's show page
+  # And I see the name of that newly added ride on this mechanic's show page.
+  
 
 
 
